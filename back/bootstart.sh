a556aefb21a9a9a9eabc0d0f8a9ea8284af0b3b8
Liuchuwei <liuchw3@mail2.sysu.edu.cn>
2024-04-25 20:24:10 +0800

    update

--- back/bootstart.sh
+++
@@ -1,9 +0,0 @@
-# 切换到docker目录
-cd ./docker
-echo 停止并删除相关容器
-output=`docker-compose down`
-echo 更新镜像,并启动容器
-output=`docker-compose up --build -d`
-echo 删除未被使用的镜像
-output=`docker image prune -f`
-echo 项目启动完成
